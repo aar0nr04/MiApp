f1bb26065160930f5c1734bd9c3ed6f2a1ceafe3
aar0nr04 <aar0nr@hotmail.com>
2022-10-20 08:46:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("Mi App")
 st.header("Esto es solo un ejemplo")
-st.sidebar("http://fcq.uach.mx/images/institucionales/")
+st.sidebar("http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg")
 boton = st.button()
 if boton:
-  st.balloon()
+  st.balloons()
   
