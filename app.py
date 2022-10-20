31d9e33988e574429cddbb07b5bdf7446eb63af4
aar0nr04 <aar0nr@hotmail.com>
2022-10-20 08:56:27 -0600

    .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Mi App")
 st.header("Esto es solo un ejemplo")
-st.sidebar("http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg")
+#st.sidebar("http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg")
 boton = st.button("Globos")
 if boton:
   st.balloons()
