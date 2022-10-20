c1381f667f1ce11353c23e22ea1e3c66ff667f84
aar0nr04 <aar0nr@hotmail.com>
2022-10-20 08:39:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.title("Mi App")
 st.header("Esto es solo un ejemplo")
+st.sidebar("http://fcq.uach.mx/images/institucionales/")
+boton = st.button()
+if boton:
+  st.balloon()
+  
